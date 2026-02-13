83de556a1895afa53d42646641bac7359c1eaad3
José Marcos <josemarcosdemelolima1@gmail.com>
2026-02-12 21:33:33 -0300

    initial database population

---
+++ src/main/resources/import.sql
@@ -0,0 +1,72 @@
+INSERT INTO tb_category(name) VALUES ('Livros');
+INSERT INTO tb_category(name) VALUES ('Eletrônicos');
+INSERT INTO tb_category(name) VALUES ('Computadores');
+
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('The Lord of the Rings', 90.5, 'Livro clássico de fantasia escrito por J.R.R. Tolkien, edição em português com capa dura e ilustrações.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('Smart TV', 2190.0, 'Smart TV LED 50 polegadas Full HD com Wi-Fi integrado e acesso a aplicativos de streaming.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('Macbook Pro', 1250.0, 'Notebook Apple Macbook Pro com processador Intel Core i5, 8GB RAM e SSD 256GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer', 1200.0, 'Computador gamer básico com Intel Core i5, 8GB RAM, GTX 1050 e HD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('Rails for Dummies', 100.99, 'Livro introdutório sobre Ruby on Rails, ideal para quem deseja aprender desenvolvimento web.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Ex', 1350.0, 'PC Gamer com Intel Core i7, 16GB RAM, GTX 1660 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/6-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer X', 1350.0, 'PC Gamer com Ryzen 5, 16GB RAM, RTX 2060 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/7-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Alfa', 1850.0, 'PC Gamer avançado com Intel Core i7, 32GB RAM, RTX 3060 e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/8-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Tera', 1950.0, 'PC Gamer com Ryzen 7, 32GB RAM, RTX 3070 e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/9-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Y', 1700.0, 'PC Gamer com Intel Core i5, 16GB RAM, GTX 1650 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/10-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Nitro', 1450.0, 'PC Gamer com Ryzen 5, 16GB RAM, GTX 1660 Super e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/11-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Card', 1850.0, 'PC Gamer com Intel Core i7, 16GB RAM, RTX 2060 e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/12-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Plus', 1350.0, 'PC Gamer com Ryzen 5, 16GB RAM, GTX 1650 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/13-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Hera', 2250.0, 'PC Gamer com Intel Core i9, 32GB RAM, RTX 3070 e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/14-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Weed', 2200.0, 'PC Gamer com Ryzen 7, 32GB RAM, RTX 3060 Ti e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/15-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Max', 2340.0, 'PC Gamer com Intel Core i9, 64GB RAM, RTX 3080 e SSD 2TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/16-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Turbo', 1280.0, 'PC Gamer com Ryzen 5, 16GB RAM, GTX 1650 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/17-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Hot', 1450.0, 'PC Gamer com Intel Core i5, 16GB RAM, GTX 1660 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/18-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Ez', 1750.0, 'PC Gamer com Ryzen 7, 32GB RAM, RTX 2060 e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/19-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Tr', 1650.0, 'PC Gamer com Intel Core i7, 16GB RAM, GTX 1660 Super e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/20-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Tx', 1680.0, 'PC Gamer com Ryzen 5, 16GB RAM, RTX 2060 e SSD 512GB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/21-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Er', 1850.0, 'PC Gamer com Intel Core i7, 32GB RAM, RTX 3060 e SSD 1TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/22-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Min', 2250.0, 'PC Gamer com Ryzen 9, 64GB RAM, RTX 3070 Ti e SSD 2TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/23-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Boo', 2350.0, 'PC Gamer com Intel Core i9, 32GB RAM, RTX 3080 e SSD 2TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/24-big.jpg');
+INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Foo', 4170.0, 'PC Gamer edição especial com Intel Core i9, 64GB RAM, RTX 3090 e SSD 2TB.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg');
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (6, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (7, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (8, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (9, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (10, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (11, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (12, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (13, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (14, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (15, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (16, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (17, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (18, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (19, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (20, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (21, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (22, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (23, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (24, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
+
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('José', 'jose@gmail.com', '999999999', '123456', '2006-06-10');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Victória', 'victoria@gmail.com', '988888888', '123456', '2007-03-17');
+
+
+INSERT INTO tb_order (moment, status, client_id) VALUES (TIMESTAMP WITH TIME ZONE '2026-02-10T13:00:00Z', 1, 1);
+INSERT INTO tb_order (moment, status, client_id) VALUES (TIMESTAMP WITH TIME ZONE '2026-02-11T15:50:00Z', 3, 2);
+INSERT INTO tb_order (moment, status, client_id) VALUES (TIMESTAMP WITH TIME ZONE '2026-02-12T14:20:00Z', 0, 1);
+
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (1, 1, 2, 90.5);
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (1, 3, 1, 1250.0);
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (2, 3, 1, 1250.0);
+INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (3, 1, 1, 90.5);
+
+INSERT INTO tb_payment (order_id, moment) VALUES (1, TIMESTAMP WITH TIME ZONE '2026-02-10T14:00:00Z');
+INSERT INTO tb_payment (order_id, moment) VALUES (2, TIMESTAMP WITH TIME ZONE '2026-02-11T16:30:00Z'); 
