9b4b76de6e24166dbed0f8eb2feec7bad5279878
José Marcos <josemarcosdemelolima1@gmail.com>
2026-02-18 11:07:53 -0300

    Create ProductRepository

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.devsuperior.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value="/products")
 public class ProductController {
 
+	@Autowired
+	ProductRepository repository;
+	
 	@GetMapping
 	public String test() {
-		return "Hello world!";
+		Optional<Product> opt = repository.findById(1L);
+		Product obj = opt.get();
+		return obj.getName();
 	}
 }
