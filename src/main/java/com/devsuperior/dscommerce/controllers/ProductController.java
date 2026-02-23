04baaaf32f9a56462cbce2e622a866600951b5d6
José Marcos <josemarcosdemelolima1@gmail.com>
2026-02-23 07:44:27 -0300

    Create product search with pagination

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,26 +1,31 @@
 package com.devsuperior.dscommerce.controllers;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value="/products")
 public class ProductController {
 
 	@Autowired
-	ProductRepository repository;
+	ProductService service;
 	
-	@GetMapping
-	public String test() {
-		Optional<Product> opt = repository.findById(1L);
-		Product obj = opt.get();
-		return obj.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+	@GetMapping()
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 }
